8afba2dfc6a4ef7d260e21373fa8287d9b84bab4
Ianc Radu <iancradu10@gmail.com>
2024-03-13 00:17:38 +0200

    end of training project

---
+++ dummy_db.py
@@ -0,0 +1,28 @@
+from datetime import date
+posts = [
+    {
+        'slug': 'hike-in-the-mountain',
+        'image': 'mountains.jpg',
+        'author': 'Max',
+        'date': date(2024, 3, 3),
+        'title': 'Mountain Hiking',
+        'excerpt': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusantium eos unde quibusdam omnis voluptates non eveniet dignissimos vitae maxime, libero reprehenderit nemo? Sapiente odio voluptatum, at minima exercitationem recusandae.',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusantium eos unde quibusdam omnis voluptates non eveniet dignissimos vitae maxime, libero reprehenderit nemo? Sapiente odio voluptatum, at minima exercitationem recusandae.'
+    },
+    {
+        'slug': 'follow-the-wind',
+        'image': 'woods.jpg',
+        'author': 'Dose',
+        'date': date(2024, 3, 3),
+        'title': 'follow the wind',
+        'excerpt': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusantium eos unde quibusdam omnis voluptates non eveniet dignissimos vitae maxime, libero reprehenderit nemo? Sapiente odio voluptatum, at minima exercitationem recusandae.',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusantium eos unde quibusdam omnis voluptates non eveniet dignissimos vitae maxime, libero reprehenderit nemo? Sapiente odio voluptatum, at minima exercitationem recusandae.'
+    },
+    {
+        'slug': 'walk-on-the-sun',
+        'image': 'coding.jpg',
+        'author': 'Sil',
+        'date': date(2024, 3, 8),
+        'title': 'Walk on the sun',
+        'excerpt': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusantium eos unde quibusdam omnis voluptates non eveniet dignissimos vitae maxime, libero reprehenderit nemo? Sapiente odio voluptatum, at minima exercitationem recusandae.',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusantium eos unde quibusdam omnis voluptates non eveniet dignissimos vitae maxime, libero reprehenderit nemo? Sapiente odio voluptatum, at minima exercitationem recusandae.'}]
