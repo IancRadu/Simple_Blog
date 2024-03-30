52b32e0f833e71cd6662be925d96f2b089e3cc20
Ianc Radu <iancradu10@gmail.com>
2024-03-30 18:57:53 +0200

    integrate database models inside the views and html templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def address(self):
+        return f"{self.address}"
+
+    def __str__(self):
+        return f"{self.full_name()}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(
+        "Author", on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField("Tag")
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=150)
+    date = models.DateField(auto_now=True, auto_now_add=False)
+
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+
+    # def __str__(self) -> str:
+    #     return f"{self.title}"
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return f"{self.caption}"
